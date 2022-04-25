022254635cf1bc925252eae96c0417423020bb6c
Matteo Vicenzi <64699641+mvicenzi@users.noreply.github.com>
2022-04-25 15:46:28 +0200

    added extract/cut flow into gui

--- run_GUI.py
+++ run_GUI.py
@@ -4,6 +4,7 @@ import sys
 import os.path
 
 from MergePDF import AppendPDFs
+from ExtractPDF import ExtractPDFs
 
 #-------------------------------------------------------------------------------
 # MERGE WINDOW: allows to select file to needs to be merged
@@ -53,8 +54,54 @@ def run_merge():
 #-------------------------------------------------------------------------------
 #EXTRACT WINDOW:
 
+extract_layout = [ [sg.Text('Select PDF file:')],
+                 [sg.In(size=(50,10), key="-FILE-"), sg.FileBrowse(key="-brFile-", file_types=(("PDF files", "*.pdf"),))],
+                 [sg.Text('A new PDF will be extracted from the interval [start, end], limits are included.\nTo select a single page, set the start and end page equal.\nPage numbering starts from 1.')],
+                 [sg.Text('Starting page:')],
+                 [sg.Radio("First page", "Radio1", key="-STARTD-", default=True), sg.Radio("Custom:","Radio1", key="-STARTC-"), sg.Input(size=(5,10), key="-START-")],
+                 [sg.Text('Ending page:')],
+                 [sg.Radio("Last page", "Radio2", key="-ENDD-", default=True), sg.Radio("Custom:","Radio2", key="-ENDC-"), sg.Input(size=(5,10), key="-END-")],
+                 [sg.Text('Save output as:')],
+                 [sg.In(size=(50,10), key="-OUT-"), sg.FileBrowse(key="-brOUT-", file_types=(("PDF files", "*.pdf"),))],
+                 [sg.Button('Extract'), sg.Button('Cancel')] ]
+
 def run_extract():
-    sg.popup("Hello here!")
+    extract_window = sg.Window('PDF Tools - Extract/Cut', extract_layout, size=(500,350))
+
+    while True:
+        extract_event, extract_values = extract_window.read()
+
+        if extract_event == sg.WIN_CLOSED or extract_event == 'Cancel':
+            extract_window.close()
+            exit()
+
+        if extract_event == 'Extract':
+            ifile = extract_values["-FILE-"]
+            ofile = extract_values["-OUT-"]
+
+            start_page_number = 1 #first page by default
+            end_page_number = -1 #last page by default
+
+            if extract_values['-STARTC-']:
+                if( extract_values['-START-'].isdigit() ):
+                    start_page_number = int(extract_values['-START-'])
+                else:
+                    sg.popup("Missing input: select a valid starting page")
+                    continue
+
+            if extract_values['-ENDC-']:
+                if( extract_values['-END-'].isdigit() ):
+                    end_page_number = int(extract_values['-END-'])
+                else:
+                    sg.popup("Missing input: select a valid ending page")
+                    continue
+
+            ExtractPDFs(ifile, start_page_number, end_page_number, ofile)
+            extract_window.close()
+            sg.popup("File extracted in " + ofile)
+            exit()
+        else:
+            sg.popup("Missing inputs: select a valid interval and the output name!")
 
 #-------------------------------------------------------------------------------
 #ROTATE WINDOW:
@@ -91,11 +138,11 @@ if __name__ == "__main__":
                 window.close()
                 run_merge()
 
-            elif values["operation"][0] == "Extract/Cut":
+            elif values["-ACTION-"][0] == "Extract/Cut":
                 window.close()
                 run_extract()
 
-            elif values["operation"][0] == "Rotate":
+            elif values["-ACTION-"][0] == "Rotate":
                 window.close()
                 run_rotate()
 
